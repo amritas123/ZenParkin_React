52c6bc7808a91d57c101b56b9fa8b9394586d7ff
Amrita Srivastava <email.amritas@gmail.com>
2017-10-31 21:20:30 +0530

    auth completed

--- zenparkin/src/HomeScreen.js
+++ zenparkin/src/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {
   Alert,
   Button,
+  Image,
+  Linking,
   Platform,
   StyleSheet,
   Text,
@@ -9,43 +11,73 @@ import {
   View,
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 console.log('inside home screen');
 
-const HomeScreen = ({ navigation }) => (
-    <View>
-        <Text>Chat with {navigation.state.params.user}</Text>
-    </View>
+const HomeScreen = ({ navigation }) => (		 
+  <View>		
+    <Text>Chat with</Text>
+  </View>		
 );
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'powderblue',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 20,
+  },
+  button: {
+    width: 200,
+    alignItems: 'center',
+    backgroundColor: '#ed42d0',
+    borderRadius: 5,
+  },
+  buttonText: {
+    padding: 20,
+    fontSize: 20,
+    color: 'white'
+  },
+  content: {
       flex: 1,
-      justifyContent: 'center',
+      justifyContent: 'flex-start',
       alignItems: 'center',
-      backgroundColor: 'powderblue',
     },
-    welcome: {
+    avatar: {
+      margin: 20,
+    },
+    avatarImage: {
+      borderRadius: 50,
+      height: 100,
+      width: 100,
+    },
+    header: {
       fontSize: 20,
       textAlign: 'center',
       margin: 10,
     },
-    instructions: {
+    text: {
       textAlign: 'center',
-      color: '#333333',
-      marginBottom: 20,
+      color: '#333',
+      marginBottom: 5,
     },
-    button: {
-      width: 200,
-      alignItems: 'center',
-      backgroundColor: '#ed42d0',
-      borderRadius: 5,
+    buttons: {
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      margin: 20,
+      marginBottom: 30,
     },
-    buttonText: {
-      padding: 20,
-      fontSize: 20,
-      color: 'white'
-    }
 });
